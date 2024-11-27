dae9a6469752a6a83950089a28d57a29d4015e42
Phelix <phelixmbani@gmail.com>
2024-11-27 12:57:30 +0300

    fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length ==1:
+        print([0])
+    else:
+        fib = [0,1]
+        
+        for i in range(2, length):
+            next_number = fib[i-1] + fib[i-2]
+            fib.append(next_number)
+            
+        print (fib)
+print_fibonacci(8)
